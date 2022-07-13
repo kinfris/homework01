47cdfceb9a9517f4a0faf95321e15db05e0713cb
Yura <Panstialei>
2022-07-13 14:05:10 +0300

    add params to err

--- src/lesson01/middlewares/input-validation-middleware.ts
+++ src/lesson01/middlewares/input-validation-middleware.ts
@@ -4,8 +4,11 @@ import {validationResult} from 'express-validator';
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});
-    } else{
+        return res.status(400).json({
+            message: errors.array()[0].msg,
+            field: errors.array()[0].param
+        })
+    } else {
         next()
     }
 }
