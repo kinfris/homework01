45af9a63739603e46e393c2ff3356ccc33d280e4
Yura <Panstialei>
2022-07-14 15:53:22 +0300

    some fix

--- src/lesson01/middlewares/input-validation-middleware.ts
+++ src/lesson01/middlewares/input-validation-middleware.ts
@@ -5,7 +5,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({
-            errorsMessages: [errors.array().map(err => ({
+            errorsMessages: [...errors.array().map(err => ({
                 message: err.msg,
                 field: err.param
             }))]
