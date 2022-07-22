7e55fc9d287524b982cfaa833e3be450f1d33026
Yura <Panstialei>
2022-07-22 16:49:10 +0300

    add auth

--- src/lesson01/middlewares/auth-middleware.ts
+++ src/lesson01/middlewares/auth-middleware.ts
@@ -4,19 +4,21 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const authInfo = req.headers.authorization
 
     if (!req.headers.authorization) {
-        res.status(401).send()
+        res.sendStatus(401)
     }
 
     if (authInfo) {
-        let loginPasswordBase64 = authInfo.split(" ")[1];
+        let loginPasswordBase64 = authInfo.split(" ");
 
-        const buff = Buffer.from(loginPasswordBase64, 'base64');
+        const buff = Buffer.from(loginPasswordBase64[1], 'base64');
 
         const str = buff.toString('utf-8');
 
         let loginPassword = str.split(":")
         if (loginPassword[0] === "admin" && loginPassword[1] === "qwerty") {
             next();
+        } else if(loginPasswordBase64[0] !== "Basic"){
+            res.sendStatus(401)
         } else {
             res.status(401).send({
                 message: "You are not authorized",
