eaa5a522c2fb160fbca9ef224068e4d30f86cd98
Yura <Panstialei>
2022-07-22 16:58:07 +0300

    add auth

--- src/lesson01/middlewares/auth-middleware.ts
+++ src/lesson01/middlewares/auth-middleware.ts
@@ -3,11 +3,7 @@ import {Request, Response, NextFunction} from "express";
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authInfo = req.headers.authorization
 
-    if (!req.headers.authorization) {
-        res.sendStatus(401)
-    }
-
-    if (!req.headers) {
+    if (!req.headers.authorization || !req.headers) {
         res.sendStatus(401)
     }
 
